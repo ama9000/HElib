618199917cd2ffb774c8de99b78d863154bb18b4
ama9000 <asma.oufi@hotmail.com>
2017-09-14 20:49:03 -0400

    successful encrypt and decrypt

--- src/secure_decision_tree/main.cpp
+++ src/secure_decision_tree/main.cpp
@@ -2,26 +2,14 @@
 // Created by ph on 9/14/17.
 //
 
-
+#include "Crypto.h"
+#include "SecComparison.h"
 #include "util.h"
 #include "Node.h"
+#include <iostream>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+using namespace std;
 
 
 
@@ -31,7 +19,47 @@ int main() {
     cout << "Program Started!!" << endl;
     cout << endl;
 
-    cout << Helper::getRandomInt(0,15) << endl;
+    //cout << Helper::getRandomInt(0,15) << endl;
+
+    HEparams params;
+    Crypto* cryptoObj = new Crypto(&params);
+    //SecComparison secComparison;
+
+    Ctxt a(*(cryptoObj->get_pubKey()));
+
+//    Ctxt b(cryptoObj.get_pubKey());
+//    Ctxt h(cryptoObj.get_pubKey());
+
+   // Ctxt *a, *b, *h;
+//
+    //cryptoObj->encrypt(a, to_ZZX(1));
+
+    cryptoObj->get_pubKey()->Encrypt(a, to_ZZX(1));
+
+    cout << to_ZZX(1)<< endl;
+
+    ZZX result;
+
+    //cryptoObj->decrypt(result, a);
+
+    cryptoObj->get_secKey()->Decrypt(result, a);
+
+    cout << result[0] << endl;
+
+
+    //cryptoObj.encrypt(b, to_ZZX(1));
+
+//
+//    //h = secComparison.OR(h, a, b);
+//
+//    //ZZX result =  to_ZZX(1);
+//
+    //ZZX result = cryptoObj.HEdecrypt(b);
+
+    //cout << endl << result << endl;
+
+
+
 
 
     //tree tree
